9efc478648c4d142282ccc927ccf4fca8aece44d
Jesper941 <>
2023-12-04 00:21:12 +0000

    hola

---
+++ run.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/location")
+def location():
+    return render_template("location.html")
+
+
+@app.route("/booking")
+def booking():
+    return render_template("booking.html")
+
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
