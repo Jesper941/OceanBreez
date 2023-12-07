4e5ce11be6c62de6cd16fdc7687e2e11dfe9f4ff
Jesper941 <>
2023-12-07 01:44:18 +0000

    pls

--- run.py
+++ run.py
@@ -1,39 +1,29 @@
 import os
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
-
+app.secret_key = os.environ.get("SECRET_KEY")
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html")
-
-
-@app.route("/location")
-def location():
-    return render_template("location.html")
-
 
 @app.route("/booking")
 def booking():
     return render_template("booking.html")
 
-
 @app.route("/menu")
 def menu():
     return render_template("menu.html")
-
 
 if __name__ == "__main__":
     app.run(
