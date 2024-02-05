4c62388d3758b75db398c9a19de8fca0ae75bc60
Jesper941 <>
2024-02-05 03:08:46 +0000

    Try to get Heroku working

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'bookings', 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'bookings', 'static')]
+STATICFILES_DIRS = []
 
 
 # Default primary key field type
