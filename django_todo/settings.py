72eca741067371835097141ea74346b96827e5f9
Jesper941 <>
2024-02-05 05:35:37 +0000

    Tro to get Heroku to work

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p*--*1+f95g*tk(^jof9yq41kr5-)snze_dq^%_^j=vtn@%ub('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
