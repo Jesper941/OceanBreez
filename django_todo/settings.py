a4f181f28b33be19131d7e17c45469b0b0dc395e
Jesper941 <>
2024-02-05 03:20:48 +0000

    Try to get Heroku working

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'bookings', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'templates')
 STATICFILES_DIRS = []
 
 
